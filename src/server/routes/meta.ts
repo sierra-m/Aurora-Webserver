2abf5f84aa4658a881e9e9c02394d513412a7a3f
Sierra MacLeod <sierra.m.macleod@gmail.com>
2024-06-26 01:01:48 -0700

    meta: reformat for ts

--- src/server/routes/meta.ts
+++ src/server/routes/meta.ts
@@ -23,188 +23,207 @@
 */
 
 import * as express from 'express'
-import {query} from '../util/pg'
-import moment from 'moment'
+import {type FlightRegistryQuery, query} from '../util/pg'
+import * as dayjs from "dayjs";
 import * as config from '../config'
 import {getFlightByUID} from '../util/uid'
+import type ModemList from "../util/modems.ts";
 
 
-const router = express.Router();
-router.modemList = undefined;  // type: ModemList
+export default class MetaRoute {
+    router: express.Router;
+    modemList: ModemList;
 
-router.get('/modems', async (req, res, next) => {
-    try {
-        const modems = router.modemList.getRedactedSet();
-        await res.json(modems);
-    } catch (e) {
-        console.log(e);
-        next(e);
+    constructor (modemList: ModemList) {
+        this.router = express.Router();
+        this.modemList = modemList;
+
+        this.router.get('/modems', this.handleModems);
+        this.router.get('/flights', this.handleFlights);
+        this.router.get('/search', this.handleSearch);
+        this.router.get('/active', this.handleActive);
     }
-});
 
-router.get('/flights', async (req, res, next) => {
-    try {
-        if ('modem_name' in req.query && req.query.modem_name !== null && typeof req.query.modem_name === 'string') {
-            const modem = router.modemList.getByName(req.query.modem_name);
-
-            if (!modem) {
-                await res.status(404).json({err: `Invalid modem name '${req.query.modem_name}'`});
-                return;
-            }
-
-            let result = await query(
-                'SELECT * FROM public."flight-registry" WHERE imei=$1',
-                [modem.imei]
-            );
-
-            let flights = result.map(x => ({date: x.start_date, uid: x.uid}));
-            res.json(flights);
-        } else {
-            res.sendStatus(400);
+    async handleModems (req: express.Request, res: express.Response, next: express.NextFunction) {
+        try {
+            const modems = this.modemList.getRedactedSet();
+            res.json(modems);
+        } catch (e) {
+            console.log(e);
+            next(e);
         }
-    } catch (e) {
-        console.log(e);
-        next(e);
     }
-});
 
-router.get('/search', async (req, res, next) => {
-    try {
-        const conditions = [];
-        const has_modem_name = req.query.modem_name != null;
-        // Only search by org if modem name not provided
-        const has_org = req.query.org != null && !has_modem_name;
-        const has_date = req.query.date != null;
-        const has_end_date = req.query.end_date != null;
-        if (has_modem_name) {
-            const modem = router.modemList.getByName(req.query.modem_name);
+    async handleFlights (req: express.Request, res: express.Response, next: express.NextFunction) {
+        try {
+            if ('modem_name' in req.query && req.query.modem_name !== null && typeof req.query.modem_name === 'string') {
+                const modem = this.modemList.getByName(req.query.modem_name);
 
-            if (!modem) {
-                await res.status(404).json({err: `Invalid modem name '${req.query.modem_name}'`});
-                return;
+                if (!modem) {
+                    res.status(404).json({err: `Invalid modem name '${req.query.modem_name}'`});
+                    return;
+                }
+
+                let result = await query<FlightRegistryQuery>(
+                    'SELECT * FROM public."flight-registry" WHERE imei=$1',
+                    [modem.imei]
+                );
+
+                let flights = result.map(x => ({date: x.start_date, uid: x.uid}));
+                res.json(flights);
+            } else {
+                res.sendStatus(400);
             }
-            conditions.push({
-                stmt: `{table}.imei = {param}`,
-                value: modem.imei
-            });
+        } catch (e) {
+            console.log(e);
+            next(e);
         }
-        if (has_org) {
-            const modems = router.modemList.getByOrg(req.query.org);
-            if (modems.length === 0) {
-                await res.status(404).json({err: `No modems found for org '${req.query.org}'`});
-                return;
-            }
-            conditions.push({
-                stmt: `{table}.imei = ANY({param}::int[])`,
-                value: modems
-            });
+    }
+
+    async handleSearch (req: express.Request, res: express.Response, next: express.NextFunction) {
+        interface SearchParameters {
+            modem_name?: string,
+            org?: string,
+            date?: string,
+            end_date?: string,
         }
-        if (has_date) {
-            if (!moment(req.query.date, "YYYY-MM-DD", true).isValid()) {
-                await res.status(400).json({err: `Date not in form 'YYYY-MM-DD'`});
-                return;
-            }
-            if (has_end_date) {
-                if (!moment(req.query.end_date, "YYYY-MM-DD", true).isValid()) {
-                    await res.status(400).json({err: `End date not in form 'YYYY-MM-DD'`});
+        try {
+            const searchQuery: SearchParameters = req.query;
+            const conditions: Array<{stmt: string, value: string | number | Array<number>}> = [];
+            const has_modem_name = searchQuery?.modem_name != null;
+            // Only search by org if modem name not provided
+            const has_org = searchQuery?.org != null && !has_modem_name;
+            const has_date = searchQuery?.date != null ;
+            const has_end_date = searchQuery?.end_date != null;
+            if (has_modem_name) {
+                const modem = this.modemList.getByName(searchQuery.modem_name!);
+
+                if (!modem) {
+                    res.status(404).json({err: `Invalid modem name '${searchQuery.modem_name}'`});
                     return;
                 }
                 conditions.push({
-                    stmt: `{table}.start_date >= {param}`,
-                    value: req.query.date
+                    stmt: `{table}.imei = {param}`,
+                    value: modem.imei
                 });
+            }
+            if (has_org) {
+                const modems = this.modemList.getByOrg(searchQuery.org!);
+                if (modems.length === 0) {
+                    res.status(404).json({err: `No modems found for org '${searchQuery.org}'`});
+                    return;
+                }
                 conditions.push({
-                    stmt: `{table}.start_date <= {param}`,
-                    value: req.query.end_date
-                });
-            } else {
-                conditions.push({
-                    stmt: `{table}.start_date = {param}`,
-                    value: req.query.date
+                    stmt: `{table}.imei = ANY({param}::int[])`,
+                    value: modems.map(x => x.imei)
                 });
             }
+            if (has_date) {
+                if (!dayjs(searchQuery.date!, "YYYY-MM-DD", true).isValid()) {
+                    res.status(400).json({err: `Date not in form 'YYYY-MM-DD'`});
+                    return;
+                }
+                if (has_end_date) {
+                    if (!dayjs(searchQuery.end_date!, "YYYY-MM-DD", true).isValid()) {
+                        res.status(400).json({err: `End date not in form 'YYYY-MM-DD'`});
+                        return;
+                    }
+                    conditions.push({
+                        stmt: `{table}.start_date >= {param}`,
+                        value: searchQuery.date!
+                    });
+                    conditions.push({
+                        stmt: `{table}.start_date <= {param}`,
+                        value: searchQuery.end_date!
+                    });
+                } else {
+                    conditions.push({
+                        stmt: `{table}.start_date = {param}`,
+                        value: searchQuery.date!
+                    });
+                }
+            }
+            if (conditions.length === 0) {
+                res.status(400).json({err: `Must give at least one condition`});
+                return;
+            }
+            const formatted = conditions.map((value, index) => {
+                const param = `$${index+1}`;
+                return value.stmt.replace('{param}', param).replace('{table}', `r`);
+            });
+            const condition_set = formatted.join(' AND ');
+            const values =  conditions.map((cond) => (cond.value));
+
+            const result = await query(
+                `SELECT DISTINCT ON (f.uid) f.uid, f.datetime, f.latitude, f.longitude, r.imei ` +
+                `FROM public."flights" f INNER JOIN public."flight-registry" r ON f.uid = r.uid ` +
+                `WHERE ${condition_set} ORDER BY f.uid, f.datetime ASC`,
+                values
+            );
+
+            const redactedResult = result.map((item) => ({
+                uid: item.uid,
+                modem: this.modemList.getRedacted(parseInt(item.imei)),
+                startPoint: {
+                    dt: item.datetime,
+                    lat: item.latitude,
+                    lng: item.longitude,
+                }
+            }));
+
+            redactedResult.sort((a, b) => {
+                if (a.modem.name < b.modem.name) {
+                    return -1;
+                }
+                if (a.modem.name > b.modem.name) {
+                    return 1;
+                }
+                return 0;
+            });
+
+            await res.status(200).json({
+                found: redactedResult.length,
+                results: redactedResult
+            });
+        } catch (e) {
+            console.log(e);
+            next(e);
         }
-        if (conditions.length === 0) {
-            await res.status(400).json({err: `Must give at least one condition`});
-            return;
-        }
-        const formatted = conditions.map((value, index) => {
-            const param = `$${index+1}`;
-            return value.stmt.replace('{param}', param).replace('{table}', `r`);
-        });
-        const condition_set = formatted.join(' AND ');
-        const values =  conditions.map((cond) => (cond.value));
-
-        const result = await query(
-          `SELECT DISTINCT ON (f.uid) f.uid, f.datetime, f.latitude, f.longitude, r.imei ` +
-          `FROM public."flights" f INNER JOIN public."flight-registry" r ON f.uid = r.uid ` +
-          `WHERE ${condition_set} ORDER BY f.uid, f.datetime ASC`,
-          values
-        );
-
-        const redactedResult = result.map((item) => ({
-            uid: item.uid,
-            modem: router.modemList.getRedacted(parseInt(item.imei)),
-            startPoint: {
-                dt: item.datetime,
-                lat: item.latitude,
-                lng: item.longitude,
-            }
-        }));
-
-        redactedResult.sort((a, b) => {
-            if (a.modem.name < b.modem.name) {
-                return -1;
-            }
-            if (a.modem.name > b.modem.name) {
-                return 1;
-            }
-            return 0;
-        });
-
-        await res.status(200).json({
-            found: redactedResult.length,
-            results: redactedResult
-        });
-    } catch (e) {
-        console.log(e);
-        next(e);
     }
-});
 
-router.get('/active', async (req, res, next) => {
-    try {
-        // Construct time delta of 12 hours ago, format for db query
-        const hoursAgo = moment.utc().subtract(12, 'hours').format('YYYY-MM-DD HH:mm:ss');
-        // Selects distinct UIDs but picks the latest datetime of each UID
-        // NOTE: This endpoint takes no user input, so direct query substitution is permitted
-        let result = await query(`SELECT DISTINCT ON (uid) uid, datetime FROM public."flights" WHERE datetime>='${hoursAgo}' ORDER BY uid ASC, datetime DESC`);
-
-        if (result.length > 0) {
-            //console.log(`Active flight tuples: ${result.length}`);
-
-            // Order uids into '1, 2, 3' string format
-            const point_identifiers = result.map(point => `('${point.uid}', '${point.datetime}')`).join(', ');
-
-            // Search for partial points from the list of uids
+    async handleActive (req: express.Request, res: express.Response, next: express.NextFunction) {
+        try {
+            // Construct time delta of 12 hours ago, format for db query
+            const hoursAgo = moment.utc().subtract(12, 'hours').format('YYYY-MM-DD HH:mm:ss');
+            // Selects distinct UIDs but picks the latest datetime of each UID
             // NOTE: This endpoint takes no user input, so direct query substitution is permitted
-            result = await query(`SELECT uid, datetime, latitude, longitude, altitude FROM public."flights" WHERE (uid, datetime) in (${point_identifiers}) ORDER BY datetime DESC`);
+            let result = await query(`SELECT DISTINCT ON (uid) uid, datetime FROM public."flights" WHERE datetime>='${hoursAgo}' ORDER BY uid ASC, datetime DESC`);
 
-            for (let partial of result) {
-                const {imei, start_date} = await getFlightByUID(partial.uid);
-                partial.modem = router.modemList.getRedacted(imei);
-                partial.start_date = start_date;
+            if (result.length > 0) {
+                //console.log(`Active flight tuples: ${result.length}`);
+
+                // Order uids into '1, 2, 3' string format
+                const point_identifiers = result.map(point => `('${point.uid}', '${point.datetime}')`).join(', ');
+
+                // Search for partial points from the list of uids
+                // NOTE: This endpoint takes no user input, so direct query substitution is permitted
+                result = await query(`SELECT uid, datetime, latitude, longitude, altitude FROM public."flights" WHERE (uid, datetime) in (${point_identifiers}) ORDER BY datetime DESC`);
+
+                for (let partial of result) {
+                    const {imei, start_date} = await getFlightByUID(partial.uid);
+                    partial.modem = this.modemList.getRedacted(imei);
+                    partial.start_date = start_date;
+                }
+                await res.json({
+                    status: 'active',
+                    points: result
+                })
+            } else {
+                await res.json({status: 'none'})
             }
-            await res.json({
-                status: 'active',
-                points: result
-            })
-        } else {
-            await res.json({status: 'none'})
+        } catch (e) {
+            console.log(e);
+            next(e);
         }
-    } catch (e) {
-        console.log(e);
     }
-});
-
-export default router;
+}
