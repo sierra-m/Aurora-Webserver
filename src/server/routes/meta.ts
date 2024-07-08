d7eaeb8113f8192c92a5f00da09e74858e580595
Sierra MacLeod <sierra.m.macleod@gmail.com>
2024-07-07 23:46:20 -0700

    tsconfig: change dayjs import style

--- src/server/routes/meta.ts
+++ src/server/routes/meta.ts
@@ -24,7 +24,7 @@
 
 import * as express from 'express'
 import {type FlightRegistryQuery, query} from '../util/pg'
-import * as dayjs from "dayjs";
+import dayjs from "dayjs";
 import utc from "dayjs/plugin/utc";
 import * as config from '../config'
 import {getFlightByUID} from '../util/uid'
