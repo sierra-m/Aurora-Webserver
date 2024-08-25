3261399ff0498b090df7f5b4050317480afaa33f
Sierra MacLeod <sierra.m.macleod@gmail.com>
2024-08-24 20:50:52 -0700

    map controller: debug auto-zoom

--- src/app/components/TrackerMap.tsx
+++ src/app/components/TrackerMap.tsx
@@ -187,6 +187,7 @@ const MapController = React.memo((props: MapControllerProps) => {
       map.setZoom(12);
       console.log(`Setting zoom!`);
     }
+    console.log(`Start position changed to ${props.startPosition}`);
   }, [props.startPosition]);
   return <></>
 });
